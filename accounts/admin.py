22615d6195ea9330bbf033a1fb9add9429f34960
Alok <yadavalok929@gmail.com>
2023-10-16 10:44:28 +0530

    registered models in admin file

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser, Employee, Customer
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'address', 'contact')
+    search_fields = ('email', 'address', 'contact')
+    # Add any other customization you want
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_employee')
+    search_fields = ('user__email',)
+    list_filter = ('is_employee',)
+    # Add any other customization you want
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'is_customer')
+    search_fields = ('user__email',)
+    list_filter = ('is_customer',)
+    # Add any other customization you want
+
